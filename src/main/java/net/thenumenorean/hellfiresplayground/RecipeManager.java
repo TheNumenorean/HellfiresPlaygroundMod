49eec14baa1d7fcd7ee25731f6b2947075a3844d
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-13 22:29:16 -0700

    Updated recipes

--- src/main/java/net/thenumenorean/hellfiresplayground/RecipeManager.java
+++ src/main/java/net/thenumenorean/hellfiresplayground/RecipeManager.java
@@ -1,7 +1,7 @@
 package net.thenumenorean.hellfiresplayground;
 
-import ic2.api.item.Items;
-import net.minecraft.block.Block;
+import ic2.api.item.IC2Items;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.thenumenorean.hellfiresplayground.blocks.HellfiresPlaygroundBlocks;
 import net.thenumenorean.hellfiresplayground.blocks.machines.LaserElectronManipulatorTileEntity;
@@ -15,36 +15,35 @@ public class RecipeManager {
 	}
 
 	public static void addRecipes() {
-		GameRegistry.addSmelting(HellfiresPlaygroundOres.bauxite_ore.blockID, new ItemStack(HellfiresPlaygroundItems.aluminum_ingot), 1.0F);
-		GameRegistry
-				.addSmelting(HellfiresPlaygroundOres.titanium_ore.blockID, new ItemStack(HellfiresPlaygroundItems.titanium_ingot), 1.0F);
-		GameRegistry
-				.addSmelting(HellfiresPlaygroundOres.tungsten_ore.blockID, new ItemStack(HellfiresPlaygroundItems.tungsten_ingot), 1.0F);
-		GameRegistry.addShapelessRecipe(new ItemStack(Block.cobblestone), new ItemStack(HellfiresPlaygroundItems.rubble), new ItemStack(
+		GameRegistry.addSmelting(HellfiresPlaygroundOres.bauxite_ore, new ItemStack(HellfiresPlaygroundItems.aluminum_ingot), 1.0F);
+		GameRegistry.addSmelting(HellfiresPlaygroundOres.titanium_ore, new ItemStack(HellfiresPlaygroundItems.titanium_ingot), 1.0F);
+		GameRegistry.addSmelting(HellfiresPlaygroundOres.tungsten_ore, new ItemStack(HellfiresPlaygroundItems.tungsten_ingot), 1.0F);
+		GameRegistry.addShapelessRecipe(new ItemStack(Blocks.cobblestone), new ItemStack(HellfiresPlaygroundItems.rubble), new ItemStack(
 				HellfiresPlaygroundItems.rubble));
 
 		LaserElectronManipulatorTileEntity.addRecipe(new ItemStack(HellfiresPlaygroundBlocks.aluminum_block), new ItemStack(
 				HellfiresPlaygroundBlocks.transparent_aluminum));
 
-		ItemStack eCrystal = Items.getItem("energyCrystal");
+		ItemStack eCrystal = IC2Items.getItem("energyCrystal");
 		eCrystal.setItemDamage(26);
 		GameRegistry.addShapedRecipe(
 				new ItemStack(HellfiresPlaygroundItems.phaser, 1),
 				new Object[] { "ZXC", "DA ", 'C', HellfiresPlaygroundItems.dilithium_crystal, 'Z', eCrystal, 'X',
-						Items.getItem("glassFiberCableItem"), 'D', Items.getItem("advancedAlloy"), 'A', Items.getItem("advancedCircuit") });
+						IC2Items.getItem("glassFiberCableItem"), 'D', IC2Items.getItem("advancedAlloy"), 'A',
+						IC2Items.getItem("advancedCircuit") });
 
 		GameRegistry.addShapedRecipe(new ItemStack(HellfiresPlaygroundBlocks.aluminum_block), "XXX", "XXX", "XXX", 'X', new ItemStack(
 				HellfiresPlaygroundItems.aluminum_ingot));
 
-		ItemStack lapCrystal = Items.getItem("lapotronCrystal");
+		ItemStack lapCrystal = IC2Items.getItem("lapotronCrystal");
 		lapCrystal.setItemDamage(26);
-		ItemStack miningLaser = Items.getItem("miningLaser");
+		ItemStack miningLaser = IC2Items.getItem("miningLaser");
 		miningLaser.setItemDamage(26);
 		GameRegistry.addShapedRecipe(
 				new ItemStack(HellfiresPlaygroundBlocks.laser_electron_manipulator, 1),
-				new Object[] { "XLX", "ACA", "XZX", 'C', Items.getItem("advancedMachine"), 'Z', lapCrystal, 'X',
-						Items.getItem("advancedAlloy"), 'D', Items.getItem("advancedAlloy"), 'A', Items.getItem("advancedCircuit"), 'L',
-						miningLaser });
+				new Object[] { "XLX", "ACA", "XZX", 'C', IC2Items.getItem("advancedMachine"), 'Z', lapCrystal, 'X',
+						IC2Items.getItem("advancedAlloy"), 'D', IC2Items.getItem("advancedAlloy"), 'A',
+						IC2Items.getItem("advancedCircuit"), 'L', miningLaser });
 
 	}
 
