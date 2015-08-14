ab992bcf0803b6184eaa47f86eabe9805fa04884
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-13 22:45:26 -0700

    Updated Aluminum block

--- src/main/java/net/thenumenorean/hellfiresplayground/blocks/AluminumBlock.java
+++ src/main/java/net/thenumenorean/hellfiresplayground/blocks/AluminumBlock.java
@@ -2,21 +2,18 @@ package net.thenumenorean.hellfiresplayground.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.MinecraftForge;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class AluminumBlock extends Block {
 
 	public AluminumBlock(int par1) {
-		super(par1, Material.anvil);
+		super(Material.anvil);
 
 		setHardness(20.0F); // 33% harder than diamond
-		setStepSound(Block.soundMetalFootstep);
-		setUnlocalizedName("aluminumBlock");
-		setTextureName("hellfiresplayground:aluminumBlock");
-		LanguageRegistry.addName(this, "Aluminum Block");
-		MinecraftForge.setBlockHarvestLevel(this, "pickaxe", 3);
-		this.setResistance(100.0F);
+		setStepSound(Block.soundTypeMetal);
+		setBlockName("aluminumBlock");
+		setBlockTextureName("hellfiresplayground:aluminumBlock");
+		setHarvestLevel("pickaxe", 3);
+		this.setResistance(20.0F);
 	}
 
 }
