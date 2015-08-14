1b362bc631c8177a965e7ee624026770e9d2884f
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-13 23:05:10 -0700

    Updated transparent alumnum

--- src/main/java/net/thenumenorean/hellfiresplayground/blocks/TransparentAlmuninum.java
+++ src/main/java/net/thenumenorean/hellfiresplayground/blocks/TransparentAlmuninum.java
@@ -2,21 +2,18 @@ package net.thenumenorean.hellfiresplayground.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.MinecraftForge;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class TransparentAlmuninum extends Block {
 
-	public TransparentAlmuninum(int par1) {
-		super(par1, Material.glass);
+	public TransparentAlmuninum() {
+		super(Material.glass);
 
 		setHardness(40.0F); // 33% harder than diamond
-		setStepSound(Block.soundMetalFootstep);
-		setUnlocalizedName("transparentAluminum");
-		setTextureName("hellfiresplayground:transparentAluminum");
-		LanguageRegistry.addName(this, "Transparent Aluminum");
-		MinecraftForge.setBlockHarvestLevel(this, "pickaxe", 3);
-		this.setResistance(140.0F);
+		setStepSound(Block.soundTypeMetal);
+		setBlockName("transparentAluminum");
+		setBlockTextureName("hellfiresplayground:transparentAluminum");
+		setHarvestLevel("pickaxe", 3);
+		setResistance(160.0F);
 	}
 
 	@Override
