653301915dfea58db50ab18c6d4408845ef0cb01
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-13 22:41:51 -0700

    Updated main class

--- src/main/java/net/thenumenorean/hellfiresplayground/HellfiresPlayground.java
+++ src/main/java/net/thenumenorean/hellfiresplayground/HellfiresPlayground.java
@@ -1,8 +1,7 @@
 package net.thenumenorean.hellfiresplayground;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.thenumenorean.hellfiresplayground.blocks.HellfiresPlaygroundBlocks;
 import net.thenumenorean.hellfiresplayground.blocks.ores.HellfiresPlaygroundOres;
 import net.thenumenorean.hellfiresplayground.blocks.ores.OreGenerator;
@@ -11,22 +10,16 @@ import net.thenumenorean.hellfiresplayground.entities.projectiles.EntityPhaserBo
 import net.thenumenorean.hellfiresplayground.items.HellfiresPlaygroundItems;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "hellfiresplayground", name = "HellfiresPlayground", version = "0.0.1", dependencies = "required-after:IC2")
-@NetworkMod(clientSideRequired = true)
+@Mod(modid = "hellfiresplayground", name = "HellfiresPlayground", version = "0.2.0", dependencies = "required-after:IC2")
 public class HellfiresPlayground {
-
-	@Instance(value = "HellfiresPlayground")
-	public static HellfiresPlayground instance;
 
 	@SidedProxy(clientSide = "net.thenumenorean.hellfiresplayground.client.ClientProxy", serverSide = "net.thenumenorean.hellfiresplayground.CommonProxy")
 	public static CommonProxy proxy;
@@ -40,11 +33,8 @@ public class HellfiresPlayground {
 
 		new GuiHandler();
 
-		MinecraftForge.EVENT_BUS.register(new SoundLoader());
-		instance = this;
-
 		oreGenerator = new OreGenerator();
-		GameRegistry.registerWorldGenerator(oreGenerator);
+		GameRegistry.registerWorldGenerator(oreGenerator, 0);
 
 		creativeTab = new HellfiresPlaygroundCreativeTab("HellfiresPlayground");
 		LanguageRegistry.instance().addStringLocalization("itemGroup.HellfiresPlayground", "HellfiresPlayground");
@@ -53,9 +43,9 @@ public class HellfiresPlayground {
 		config.load();
 
 		// Initialize all blocks and items
-		int currID = HellfiresPlaygroundOres.initialize(1701, config, creativeTab, oreGenerator);
-		currID = HellfiresPlaygroundBlocks.initialize(currID, config, creativeTab);
-		HellfiresPlaygroundItems.initialize(20000, config, creativeTab);
+		HellfiresPlaygroundOres.initialize(creativeTab, oreGenerator);
+		HellfiresPlaygroundBlocks.initialize(creativeTab);
+		HellfiresPlaygroundItems.initialize(creativeTab);
 
 		EntityRegistry
 				.registerModEntity(EntityPhaserBolt.class, "PhaserBolt", EntityRegistry.findGlobalUniqueEntityId(), this, 64, 1, true);
